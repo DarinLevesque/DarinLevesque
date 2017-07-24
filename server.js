c72ae0840663420e5e4d6771c0a30f3beb4d0635
Darin Levesque <darinlevesque@gmail.com>
2017-07-24 16:06:42 -0400

    add compression, security, contact form

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const compression = require('compression');
+const helmet = require('helmet');
 const express = require('express');
 const app = express();
 
+app.use(helmet());
 app.use(compression());
 
 app.use(express.static('dist'));
