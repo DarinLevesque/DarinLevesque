7fe6822935d0b8394006eb86f33d700e3a7b7522
Darin Levesque <darinlevesque@gmail.com>
2017-07-24 15:00:25 -0400

    altered port to meet Heroku's standard

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static('/dist'));
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('App listening on port ', port);
 });
